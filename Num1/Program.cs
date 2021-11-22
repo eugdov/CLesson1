e5fdb1a9f8997829a21d11357837baa56f4771e0
eugdov <eugene.dov@gmail.com>
2021-11-22 20:47:05 +0300

    First commit

---
+++ Num1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите своё число: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+Console.WriteLine("Введите степень числа: ");
+string b = Console.ReadLine();
+double bb = Convert.ToDouble(b);
+double c = Math.Pow(aa, bb);
+Console.WriteLine(c);
