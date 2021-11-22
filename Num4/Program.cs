d88f070fd7f23f4df4c19df7908235f3d7396131
eugdov <eugene.dov@gmail.com>
2021-11-22 21:34:09 +0300

    Exercise 4

---
+++ Num4/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите номер недели: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+if (aa == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (aa == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (aa == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (aa == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (aa == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (aa == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (aa == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (aa < 1 || aa > 7)
+{
+    Console.WriteLine("Такого порядкового номера дня недели не существует, выберите число от 1 до 7");
+}
