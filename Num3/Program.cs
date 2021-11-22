2338d0f0245b8df1b28e2b0504b8c1154e571412
eugdov <eugene.dov@gmail.com>
2021-11-22 21:18:36 +0300

    Exersize 3

---
+++ Num3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+Console.WriteLine("Введите второе число: ");
+string b = Console.ReadLine();
+double bb = Convert.ToDouble(b);
+if (aa > bb)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+if (aa < bb)
+{
+    Console.WriteLine("Первое число меньше второго");
+}
+if (aa == bb)
+{
+    Console.WriteLine("Первое и второе числа равны");
+}
