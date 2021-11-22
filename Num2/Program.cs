dd5023f2079119666d3e43a5a5889f3fda87c0b4
eugdov <eugene.dov@gmail.com>
2021-11-22 21:12:43 +0300

    Exersise 2

---
+++ Num2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+Console.WriteLine("Введите второе число: ");
+string b = Console.ReadLine();
+double bb = Convert.ToDouble(b);
+if (aa == Math.Sqrt(bb))
+{
+    Console.WriteLine("Да, первое число является корнем второго");
+}
+else
+{
+    Console.WriteLine("Нет, первое число не является корнем второго");
+}
+
+/*Console.WriteLine("Введите первое число: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+Console.WriteLine("Введите второе число: ");
+string b = Console.ReadLine();
+double bb = Convert.ToDouble(b);
+if (bb == aa*aa)
+{
+    Console.WriteLine("Да, первое число является корнем второго");
+}
+else
+{
+    Console.WriteLine("Нет, первое число не является корнем второго");
+}
+*/
