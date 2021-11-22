f5abcfefd426e0c3ff441cfcc8d747b541a9321e
eugdov <eugene.dov@gmail.com>
2021-11-22 21:44:10 +0300

    Exercise 5

---
+++ Num5/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число: ");
+string a = Console.ReadLine();
+double aa = Convert.ToDouble(a);
+Console.WriteLine("Введите второе число: ");
+string b = Console.ReadLine();
+double bb = Convert.ToDouble(b);
+Console.WriteLine("Введите третье число: ");
+string c = Console.ReadLine();
+double cc = Convert.ToDouble(c);
+double max = aa;
+if (max > bb)
+{
+    
+}
+else
+{
+    max = bb;
+}
+if (max > cc)
+{
+    
+}
+else
+{
+    max = cc;
+}
+Console.WriteLine("Максимальное число = " + max);
